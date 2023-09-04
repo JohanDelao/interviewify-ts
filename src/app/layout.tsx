ca92f76f41d02ceae1b59c537acad8ec65dba22e
JohanDelao <johandelao10@gmail.com>
2023-09-03 22:34:28 -0400

    welcome page + tailwind implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
 import { Suspense } from 'react';
 import Loading from './components/loading';
-
-const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '700'] });
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -18,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body>
         <Suspense fallback={<Loading />}>{children}</Suspense>
       </body>
     </html>
