042cf093a5f5638209021149b76a6eb115371d54
thunder-design <aandha8342@gmail.com>
2023-09-30 21:45:36 -0400

    developed prompt for gpt to evaluate user answers

--- server/routes/gpt.ts
+++ server/routes/gpt.ts
@@ -34,30 +34,40 @@ router.post("/evaluate", async (req, res) => {
       communication, and collaboration in resolving team conflicts.`
   }
 
+  const role = req.body.role ?? "software engineering"
+
   const systemMessage: { role: "function" | "system" | "user" | "assistant", content: string } = {
     "role": "system",
-    "content": `You are an AI career coach that will help users with Software Engineering
+    "content": `You are an AI career coach that will help users with role: ${role}
     interview behavioral questions. You will receive messages in a JSON string format with
-    a 'question' and an 'answer' attribute. Your response to the user will be a numbered 
-    list with each numbered section corresponding to 1 question. For each 
-    message that you receive, you must do the following using the question and answer of 
-    that message: Put the question at the top; Give a bulleted list of criticisms for 
-    the user. Provide thoughtful feedback as to what they did well and what they 
-    can improve; Keep your feedback concise and to the point; Score the response on a
-    scale of 1-100 with 60 being a passing score. So, a response that is not great but will
-    keep you moving along in the interview should be a 60.`
+    a 'question' and an 'answer' attribute. Your job is to evaluate the user's answer and provide feedback.
+    
+    The following instructions must be followed for all messages you receive
+
+    Your response must be an array of JSON objects with each one having the following attributes:
+
+    - 'question': A string of the question asked
+    - 'explanation': A string of the explanation of the question. Why was this question asked? What is the interviewer looking for?; max 100 words
+    - 'feedback': A string of thoughtful feedback as to what they did well and what they can improve; Keep your feedback concise and to the point; max 100 words; 
+    - 'score': An integer score on a scale of 1-100, with 60 being a passing score; a score of 60 is one that would pass the interview but has significant points of improvement; a score of 100 is a perfect answer
+
+
+    Make sure to consider the following when evaluating the user's answer: 
+    - The role the user is applying for
+    - The tone of the answer
+    - The content of the answer
+    - The clarity of the answer`
   }
 
   const userMessage: { role: "function" | "system" | "user" | "assistant", content: string } = {
     "role": "user",
     "content": JSON.stringify(exampleTranscript)
   }
-
   await openai.chat.completions.create({
     messages: [systemMessage, userMessage],
     model: "gpt-4",
   }).then((response) => {
-    res.status(200).json(response.choices[0].message.content);
+    res.status(200).json(response.choices[0]);
   })
     .catch(err => res.status(400).send({ error: `❌ Evaluation error: ${err}` }))
 })
