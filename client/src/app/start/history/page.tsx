1ddeaee4f92124ddc3ae146ac168212687c47dd1
JohanDelao <johandelao10@gmail.com>
2023-10-01 10:09:47 -0400

    lint

--- client/src/app/start/history/page.tsx
+++ client/src/app/start/history/page.tsx
@@ -9,7 +9,7 @@ export default function History() {
       <div className="flex flex-col gap-4 mx-auto md:mx-auto px-4 md:px-0 md:w-11/12 w-10/12">
         <p className="text-2xl font-bold">History</p>
       </div>
-      <div className='flex flex-col gap-2 mx-auto px-4 md:px-0 md:w-11/12 w-10/12'>
+      <div className="flex flex-col gap-2 mx-auto px-4 md:px-0 md:w-11/12 w-10/12">
         Nothing
       </div>
     </div>
