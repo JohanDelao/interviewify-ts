9326da891a91ab86f7c0c2bda8a94b53206cd847
thunder-design <aandha8342@gmail.com>
2023-11-11 17:19:34 -0500

    login error fixed

--- server/passport.ts
+++ server/passport.ts
@@ -1,34 +1,32 @@
-import * as googleAuth from "passport-google-oauth20"
-import passport from "passport"
-import dotenv from "dotenv"
-import User from "./Models/userModel"
+import * as googleAuth from 'passport-google-oauth20';
+import passport from 'passport';
+import dotenv from 'dotenv';
+import User from './Models/userModel';
 
-dotenv.config()
+dotenv.config();
 
 passport.use(
-  new googleAuth.Strategy({
-    clientID: process.env.GOOGLE_CLIENT_ID ?? "",
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-    callbackURL: '/auth/google/callback',
-    scope: ['profile', 'email']
-  },
+  new googleAuth.Strategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      callbackURL: '/auth/google/callback',
+      scope: ['profile', 'email'],
+    },
     async (_accessToken, _refreshToken, profile, done) => {
       const user = await User.findById(profile.id);
       if (user) return done(null, user);
       else {
-        let email = profile.emails ? profile.emails[0] : ""
+        let email = profile.emails ? profile.emails[0].value : '';
         const newUser = await User.create({
           username: profile.displayName,
           email: email,
           _id: profile.id,
         });
-        console.log(newUser)
         return done(null, newUser);
       }
     }
-
   )
-
 );
 
 passport.serializeUser((user: any, done) => {
@@ -36,6 +34,6 @@ passport.serializeUser((user: any, done) => {
 });
 
 passport.deserializeUser(async (id, done) => {
-  const user = await User.findById(id)
-  done(null, user)
-})
+  const user = await User.findById(id);
+  done(null, user);
+});
