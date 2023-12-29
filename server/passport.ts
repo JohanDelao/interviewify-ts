0275d30070920c86c31c8586489aedbc0581d71f
thunder-design <aandha8342@gmail.com>
2023-12-29 12:34:33 -0500

    switched userid to mongo objectid

--- server/passport.ts
+++ server/passport.ts
@@ -2,6 +2,7 @@ import * as googleAuth from 'passport-google-oauth20';
 import passport from 'passport';
 import dotenv from 'dotenv';
 import User from './Models/userModel';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -14,14 +15,14 @@ passport.use(
       scope: ['profile', 'email'],
     },
     async (_accessToken, _refreshToken, profile, done) => {
-      const user = await User.findById(profile.id);
+      let email = profile.emails ? profile.emails[0].value : '';
+      const user = await User.findOne({ email: email });
       if (user) return done(null, user);
       else {
-        let email = profile.emails ? profile.emails[0].value : '';
         const newUser = await User.create({
           username: profile.displayName,
           email: email,
-          _id: profile.id,
+          _id: new mongoose.Types.ObjectId(parseInt(profile.id)),
         });
         return done(null, newUser);
       }
